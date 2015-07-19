291b4e79ba5113596d0ac5b29125e075db7db795
icstunna <mdelostr@gmail.com>
2015-07-18 19:26:23 -0700

    created comment documentation for spec/comment file to clarify the tasks of each test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,17 +8,20 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   before :each do
+    # Clears test database prior to every test
     Post.destroy_all
     Comment.destroy_all
   end
 
   before :all do
+    # Creating post to comment association
     @associate_post = Post.create!(title: "test", content: "post")
     @associate_comment = Comment.create!(title: "test", content: "comment")
     @associate_post.comments << @associate_comment
   end
 
   describe "#comments" do
+    # Testing for association
     it "returns an array of all associated comments" do
       expect(@associate_post.comments).to eq([@associate_comment])
     end
