67cb0b907e31bdb5329a881aae11ef5db1615ea9
icstunna <mdelostr@gmail.com>
2015-07-18 19:24:51 -0700

    created test for association between the two models, testing one to many association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,5 +7,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  before :each do
+    Post.destroy_all
+    Comment.destroy_all
+  end
 
+  before :all do
+    @associate_post = Post.create!(title: "test", content: "post")
+    @associate_comment = Comment.create!(title: "test", content: "comment")
+    @associate_post.comments << @associate_comment
+  end
+
+  describe "#comments" do
+    it "returns an array of all associated comments" do
+      expect(@associate_post.comments).to eq([@associate_comment])
+    end
+  end
 end
