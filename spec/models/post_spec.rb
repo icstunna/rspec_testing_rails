4746ea68d6d05999720e11025798b2c071ef259e
icstunna <mdelostr@gmail.com>
2015-07-18 18:50:48 -0700

    created sample rspec tests for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it "creates a new instance of a post" do
-    new_post = Post.create!(title: "test", content: "post")
-    expect(new_post.title).to eq("test")
+  before :all do
+    @new_post = Post.create(title: "test", content: "post")
+  end
+
+  describe "#title" do
+    it "returns the content for title" do
+      # new_post = Post.create!(title: "test", content: "post")
+      expect(@new_post.title).to eq("test")
+    end
+  end
+
+  describe "#content" do
+    it "return the contents of content" do
+      expect(@new_post.content).to eq("post")
+    end
   end
 end
